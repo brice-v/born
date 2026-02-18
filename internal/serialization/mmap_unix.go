72706213f7cef4a62b75c705e2f044fbe73cdd47
Andy <a.kolkov@gmail.com>
2026-02-18 15:26:54 +0300

    fix: remove unused nolint on int(size) in mmap_unix.go

--- internal/serialization/mmap_unix.go
+++ internal/serialization/mmap_unix.go
@@ -12,7 +12,7 @@ func mmapFile(f *os.File, size int64) ([]byte, error) {
 	return syscall.Mmap(
 		int(f.Fd()), //nolint:gosec // G115: file descriptor fits in int
 		0,
-		int(size), //nolint:gosec // G115: file size validated by caller
+		int(size),
 		syscall.PROT_READ,
 		syscall.MAP_SHARED,
 	)
